6a699f51b2dbcdffab8e5e8e9ed4d7ecfcfc3745
Kenzo douchet <kenzodouchet33@gmail.com>
2023-11-09 08:13:25 +0100

    l

---
+++ app/admin/login_activities.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register LoginActivity do
+  index do
+    column :id
+    column :identity
+    column :user
+    column :scope
+    column :strategy
+    column :success
+    column :failure_reason
+    column :context
+    column :ip
+    column :user_agent
+    column :referrer
+    column :city
+    column :region
+    column :country
+    column :latitude
+    column :longitude
+    column :created_at
+  end
+end
