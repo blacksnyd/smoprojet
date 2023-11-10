867ae1c8d7f85cd8d3f2b8168f32231dc9790668
Kenzo douchet <kenzodouchet33@gmail.com>
2023-11-10 05:04:59 +0100

    fix bug

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -3,12 +3,11 @@ Geocoder.configure(
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :nominatim,         # name of geocoding service (symbol)
   ip_lookup: :maxmind,
-  maxmind: { service: :city, license_key: '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' }    # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: "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",               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
 
   # Exceptions that should not be rescued by default
